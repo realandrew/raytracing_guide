ee0fc37209db86854325ed3c460973c4d79220d9
Andrew Mitchell <andrew@andrewjmitchell.com>
2022-06-12 20:29:17 -0400

    Updated config to correct values.

--- pages/.vitepress/config.ts
+++ pages/.vitepress/config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   lang: 'en-US',
-  title: 'OpenGL Guide',
-  description: 'A modern guide to OpenGL (with Rust as the source language), along with a collection of modern graphics topics/examples.',
+  title: 'Raytracing Guide',
+  description: 'A modern guide to Raytracing (with Rust as the source language), along with a collection of modern graphics topics/examples.',
   appearance: true, // Enable dark theme by default (this is true by default but I want to be explicit)
   lastUpdated: true, // Display the last updated time based on git timestamp
   themeConfig: {
@@ -20,7 +20,7 @@ export default defineConfig({
       copyright: 'Copyright © 2022-present Andrew Mitchell'
     },
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/realandrew/opengl_guide' }
+      { icon: 'github', link: 'https://github.com/realandrew/raytracing_guide' }
     ]
   }
 })
